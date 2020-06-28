96a8f5701f5f73c8ea8169f224918fe940e957a4
sou-uemura <soh.mtw@gmail.com>
2020-06-28 19:39:56 +0700

    static-member

---
+++ src/static-members.ts
@@ -0,0 +1,18 @@
+export {};
+
+class Me {
+    static isProgrammer: boolean = true;
+    static firstName: string = 'Atsushi';
+    static lastName: string = 'Ishida';
+
+    static work() {
+        // return "Hey, guys! This is Atsushi! Are you interested in TypeScript? Lets dive into TypeScript!";
+        return `Hey, guys! This is ${this.firstName}! Are you interested in TypeScript? Lets dive into TypeScript!`;
+    }
+}
+
+// let me = new Me();
+// console.log(me.isProgrammer);
+
+console.log(Me.isProgrammer);
+console.log(Me.work());
